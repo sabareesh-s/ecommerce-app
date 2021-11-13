8a9081f98ec127808a822135d1865d4aba2421ac
Sabareesh S <sabareesh1221@gmail.com>
2021-11-13 19:10:44 +0530

    :')

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import axios from 'axios'
+
 import './App.css';
+import ProductListing from './components/ProductListing';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, world</h1>
-      
+      <ProductListing/>
     </div>
   );
 }
---
+++ src/components/ProductListing.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+function ProductListing() {
+    let [product, setProduct] = useState([])
+    {    useEffect(() => {
+        axios.get(`https://store.rebune.net/api/products.json`).then((response)=>{
+            
+            console.log(response.data);
+            setProduct(response.data);
+                
+        })
+     }, [])
+     }
+    return (
+        <div>
+          {product.map(products => (
+            <h1 className='title' key={products.id}>hi{ product.name }</h1>
+        ))}
+        </div>
+    )
+}
+
+export default ProductListing
